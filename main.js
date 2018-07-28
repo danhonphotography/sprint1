14ed0b3ed20c86aa86ec8c95f1b8cd3a906c982e
danhonphotography <danhonphotography@gmail.com>
2018-07-28 12:55:24 -0400

    Added prompts for js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let userName = prompt("Welcome to Dan's site...What's your name?");
 console.log(userName);
 
-let userHome = prompt("Howdy! " + userName + "Tell me, Where are you from?");
+let userHome = prompt("Howdy! " + userName + "tell me, Where are you from?");
 console.log(userHome);
 
 let userColor = prompt("Wow your all the way from " + userHome + ". That is cool." + " What is your favorite color?");
 console.log(userColor);
 
+let userFood = prompt(userColor + " is a nice color. " + "What would you say is your favorite food?")
+console.log(userFood);
 
-
+let userTotal = alert(userName + "from " + userHome + ", " +""+ userColor + " is one of my favorite colors and I also enjoy eating " + userFood +"." + " It has been great getting to know you. Thank you for stopping by.")
