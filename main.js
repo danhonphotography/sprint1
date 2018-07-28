6a96de5354247efaea14a8deb233cf646efdc594
danhonphotography <danhonphotography@gmail.com>
2018-07-28 16:39:57 -0400

    Added JS to HTMl list code

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 let userName = prompt("Welcome to Dan's site...What's your name?");
 console.log(userName);
+let elName = document.getElementById ("name");
+elName.innerHTML = userName;
 
 let userHome = prompt("Howdy! " + userName + "tell me, Where are you from?");
 console.log(userHome);
+let elHome = document.getElementById ("home");
+elHome.innerHTML = userHome;
 
 let userColor = prompt("Wow your all the way from " + userHome + ". That is cool." + " What is your favorite color?");
 console.log(userColor);
+let elColor = document.getElementById ("color");
+elColor.innerHTML = userColor;
 
 let userFood = prompt(userColor + " is a nice color. " + "What would you say is your favorite food?")
 console.log(userFood);
+let elFood = document.getElementById ("food")
+elFood.innerHTML =userFood;
 
 let userTotal = alert(userName + "from " + userHome + ", " +""+ userColor + " is one of my favorite colors and I also enjoy eating " + userFood +"." + " It has been great getting to know you. Thank you for stopping by.")
